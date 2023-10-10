51955eab81541ae39af125755502e79fb018ff66
Jan Bölsche <regular.gonzales@googlemail.com>
2023-10-10 12:11:25 +0200

    Query scan events, joined with barcodeables

--- index.js
+++ index.js
@@ -7,35 +7,51 @@ const toPull = require('stream-to-pull-stream')
 
 const Queries = require('./queries')
 
-const connection = mysql.createConnection(Object.assign(require('../credentials.json'), {
+const connectionOpts = Object.assign(require('../credentials.json'), {
   dateStrings: true,
+  connectionLimit: 10,
   ssl: {
     ca: fileContent('ca-cert.pem'),
     cert: fileContent('client-cert.pem'),
     key: fileContent('client-key.pem')
   }
-}))
+})
 
-const {bookings} = Queries(connection)
+const connection = mysql.createConnection(connectionOpts)
+
+const {bookings, entries} = Queries(connection)
 
 const from = '2023-10-02 00:00:00'
 const to   = '2023-10-03 00:00:00'
 
-const stream = bookings(from, to)
-  .stream({highWaterMark: 50})
+function showBookings() {
+  pull(
+    src(bookings(from, to)),
+    pull.drain(({id, created_at, product_name}) => {
+      console.log(`${id} ${created_at} ${product_name}`)
+    })
+  )
+}
 
-pull(
-  toPull.source(stream),
+function showEntries() {
+  pull(
+    src(entries(from, to)),
+    pull.drain(({foreign_type, foreign_id, entry_at, people_count, position, booking_id, start_time}) => {
+      console.log(`${entry_at} ${people_count}x ${foreign_type}#${foreign_id} @${Buffer.from(position, 'base64').toString()} ${booking_id} ${start_time}`)
+    })
+  )
+}
 
-  pull.drain(({id, created_at, product_name}) => {
-    console.log(`${id} ${created_at} ${product_name}`)
-  })
-)
+showEntries()
 
 connection.end()
 
 // -- util
 
+function src(s) {
+  const stream = s.stream({highWaterMark: 50})
+  return toPull.source(stream)
+}
 
 function fileContent(fname) {
   return fs.readFileSync(join(__dirname, '..', fname))
--- queries.js
+++ queries.js
@@ -1,7 +1,60 @@
+const snakeCase = require('just-snake-case')
+
 module.exports = function(connection) {
 
   return {
+    entries,
     bookings
+  }
+
+  function entries(from, to) {
+    const barcodeable = 'BookingSeat' // 'TicketSale' // 'Booking'
+
+    const conditions = [
+      'bse.is_entry IS TRUE',
+      'bse.is_valid IS TRUE',
+      'bse.entry_at >= ?',
+      'bse.entry_at < ?',
+
+      `bc.barcodeable_type = '${barcodeable}'`,
+    ]
+    const additional_joins = []
+    if (barcodeable == 'Booking') {
+      additional_joins.push(`
+        INNER JOIN bookings_view bo
+          ON bc.barcodeable_id = bo.id
+      `)
+    } else {
+      const table = snakeCase(barcodeable) + 's_view'
+      additional_joins.push(`
+        INNER JOIN ${table} barcodeable
+          ON bc.barcodeable_id = barcodeable.id
+        LEFT JOIN bookings_view bo
+          ON barcodeable.booking_id = bo.id
+      `)
+
+    }
+    const sql = `
+SELECT 
+  entry_at,
+  people_count,
+  position,
+  bc.barcodeable_type AS foreign_type,
+  bc.barcodeable_id AS foreign_id,
+ 
+  bo.id AS booking_id,
+  bo.start_time
+
+FROM barcode_scan_events_view bse
+INNER JOIN barcodes_view bc
+  ON bse.barcode_id = bc.id
+
+${additional_joins.join('\n')}
+
+WHERE ${conditions.join('\nAND ')}
+ORDER BY bse.entry_at
+  `
+    return connection.execute(sql, [from, to])
   }
 
   function bookings(from, to) {
@@ -26,15 +79,15 @@ SELECT
 
   FROM bookings_view b
   JOIN products_view p ON
-  b.product_id = p.id
+    b.product_id = p.id
   JOIN product_categories_view pc ON
-  pc.id = p.product_category_id
+    pc.id = p.product_category_id
   JOIN languages_view l ON
-  b.language_id = l.id 
+    b.language_id = l.id 
   JOIN customer_adresses_view ca ON
-  b.customer_adress_id = ca.id
+    b.customer_adress_id = ca.id
   JOIN countries_view c ON
-  ca.country_id = c.id
+    ca.country_id = c.id
 
   WHERE b.created_at >= ?
   AND b.created_at < ?
